59cf15787457744eca01339b099c4bdc3dbea9f3
Thulasiraj Komminar <39799163+thulasirajkomminar@users.noreply.github.com>
2024-07-16 14:05:10 +0200

    fix: removed a resource in favour of mcaf module

--- iam.tf
+++ iam.tf
@@ -3,14 +3,9 @@ module "job_execution_role" {
   source                = "github.com/schubergphilis/terraform-aws-mcaf-role?ref=v0.4.0"
   name                  = "GlueExecution-${var.name}"
   create_policy         = var.role_policy != null ? true : false
+  policy_arns           = ["arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"]
   principal_type        = "Service"
   principal_identifiers = ["glue.amazonaws.com"]
   role_policy           = var.role_policy
   tags                  = var.tags
-}
-
-resource "aws_iam_role_policy_attachment" "job_execution_role" {
-  count      = var.role_arn == null ? 1 : 0
-  role       = module.job_execution_role[0].id
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
