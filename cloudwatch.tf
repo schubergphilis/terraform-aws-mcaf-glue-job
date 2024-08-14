09e206d466a24438daf22013c4f2cc69665f2179
Thulasiraj Komminar <39799163+thulasirajkomminar@users.noreply.github.com>
2024-08-14 11:29:56 +0200

    fix: improved job logging variable
    Enhanced the job logging feature to allow flexible configuration for enabling continuous job logging.

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,4 +1,6 @@
 resource "aws_cloudwatch_log_group" "default" {
+  count = local.create_log_group ? 1 : 0
+
   name              = "/aws/glue/jobs/${var.name}"
   kms_key_id        = var.kms_key_id
   retention_in_days = var.log_retention_days
