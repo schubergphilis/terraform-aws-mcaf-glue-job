6405b8eaa7fb474f709b393c23042457cb8b7e6d
Deni Hot <dhot@schubergphilis.com>
2026-06-10 10:53:35 +0200

    update AWS provider and required Terraform version to 6.40.0 and 1.6.0

--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0.0"
+      version = ">= 6.40.0"
     }
   }
-  required_version = ">= 1.3"
+  required_version = ">= 1.6.0"
 }
--- examples/security-configuration/versions.tf
+++ examples/security-configuration/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.95.0"
+      version = ">= 6.40.0"
     }
   }
-  required_version = ">= 1.3"
+  required_version = ">= 1.6.0"
 }
